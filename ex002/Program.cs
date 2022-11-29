7d1144d8fb5f7306c2ba4445ffa110978ef4c0c3
Ilya <DriverIlya@gmail.com>
2022-11-29 20:20:24 +0300

    squre progrmme

---
+++ ex002/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("123Hello, World!");
+string input = Console.ReadLine()!;
+int parsedInput = int.Parse(input);
+
+int sqr = parsedInput * parsedInput;
+
+Console.WriteLine(sqr);

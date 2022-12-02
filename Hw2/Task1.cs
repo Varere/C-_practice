0d33bf989f292b4df489b4a5bc26e7d42f7b8bcc
Ilya <DriverIlya@gmail.com>
2022-12-02 21:56:45 +0300

    2nd homework added

--- Hw2/Task1.cs
+++ Hw2/Task1.cs
@@ -1,7 +1,13 @@
-﻿Console.WriteLine("Введите трехзначное число");
-string input = Console.ReadLine()!;
-int number = int.Parse(input);
-
+﻿
+while(1){
+    Console.WriteLine("Введите трехзначное число");
+    int number = int.Parse(Console.ReadLine()!);
+    if (number >= 100 && number < 1000) {
+        break;
+    } else {
+        Console.WriteLine("Некорректный ввод повторите попытку");
+    }
+}
 number /= 10;
 Console.WriteLine(number % 10);
 
--- ex005/Program.cs
+++ ex005/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("Введите трехзначное число");
-string input = Console.ReadLine()!;
-int n = int.Parse(input);
-
-Console.WriteLine(n % 10);   
-
-
-
+﻿int number;
+while(true){
+    Console.WriteLine("Введите трехзначное число");
+    number = int.Parse(Console.ReadLine()!);
+    if (number >= 100 && number < 1000) {
+        break;
+    } else {
+        Console.WriteLine("Некорректный ввод повторите попытку");
+    }
+}
+number /= 10;
+Console.WriteLine(number % 10);

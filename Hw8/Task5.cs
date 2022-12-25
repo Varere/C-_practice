4b21e1c67fc8ff40c2cefab2684c87c5148a75fd
Ilya <DriverIlya@gmail.com>
2022-12-25 15:32:53 +0300

    8th homework added

--- Hw8/Task5.cs
+++ Hw8/Task5.cs
@@ -4,41 +4,48 @@ Console.WriteLine("Введите сторону матрицы");
 int n = int.Parse(Console.ReadLine()!);
 
 
-int[,] futureSpiral = new int [n,n];
+int[,] futureSpiral = new int[n, n];
 FillSpiral(futureSpiral);
 Print2DArray(futureSpiral);
 
-void FillSpiral(int[,] arr) {
+void FillSpiral(int[,] arr)
+{
     int deltaI = 0;
     int deltaJ = 0;
     int startNumber = 1;
     int even = 0;
-    if(arr.GetLength(0) % 2 != 0) {
+    if (arr.GetLength(0) % 2 != 0)
+    {
         even++;
     }
-    while (arr.GetLength(0) - 2 * deltaI >= even - 2 || arr.GetLength(1) - 2 * deltaJ >= even - 2){
+    while (arr.GetLength(0) - 2 * deltaI >= even - 2 || arr.GetLength(1) - 2 * deltaJ >= even - 2)
+    {
         int tempI = deltaI;
         int tempJ = deltaJ;
-        for(int j = deltaJ; j < arr.GetLength(1) - deltaJ; j++) {
-            arr[deltaI,j] = startNumber;
+        for (int j = deltaJ; j < arr.GetLength(1) - deltaJ; j++)
+        {
+            arr[deltaI, j] = startNumber;
             startNumber++;
         }
         tempI++;
         tempJ = arr.GetLength(1) - deltaJ - 1;
-        for(int i = tempI; i < arr.GetLength(0) -  deltaI; i++) {
-            arr[i,tempJ] = startNumber;
+        for (int i = tempI; i < arr.GetLength(0) - deltaI; i++)
+        {
+            arr[i, tempJ] = startNumber;
             startNumber++;
         }
         tempJ--;
         tempI = arr.GetLength(0) - deltaI - 1;
-        for(int j = tempJ; j >= deltaJ; j--) {
-            arr[tempI,j] = startNumber;
+        for (int j = tempJ; j >= deltaJ; j--)
+        {
+            arr[tempI, j] = startNumber;
             startNumber++;
         }
         tempJ = deltaJ;
         tempI--;
-        for(int i = tempI; i > deltaI; i--) {
-            arr[i,deltaJ] = startNumber;
+        for (int i = tempI; i > deltaI; i--)
+        {
+            arr[i, deltaJ] = startNumber;
             startNumber++;
         }
         deltaI++;
